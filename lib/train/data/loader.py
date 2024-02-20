1702702d8c6c7082c9581ae61234d92b96a9984f
hojat-ardi <hojat.rd133@yahoo.com>
2024-02-20 14:03:00 +0100

    test on got-10k

--- lib/train/data/loader.py
+++ lib/train/data/loader.py
@@ -2,7 +2,7 @@ import torch
 import torch.utils.data.dataloader
 import importlib
 import collections
-from torch._six import string_classes
+from six import string_types as string_classes
 from lib.utils import TensorDict, TensorList
 int_classes = int
 
